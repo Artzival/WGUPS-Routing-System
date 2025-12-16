7248808da7f805f1a7892b583237f47256476844
Andrew <awilso33@wgu.edu>
2025-12-15 20:44:52 -0500

    fixed address dictionary

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import csv
 with open("CSVdata/distance_table_CSV.csv") as distCSV:
     DistanceCSV = csv.reader(distCSV)
     DistanceCSV = list(DistanceCSV)
-with open("CSVdata/address_list_CSV.csv") as addrCSV:
-    addressDict = []
+with open("CSVdata/address_list_CSV.csv", newline='') as addrCSV:
+    addressDict = {}
     AddressCSV = csv.DictReader(addrCSV)
     for row in AddressCSV:
-        addressDict.append(row)
+        key, value = row
+        addressDict[int(key)] = value.strip()
+
 
 
 #creating the class for the packages in the hash table
@@ -95,21 +97,21 @@ class HashTable:
 packageTable = HashTable(40)
 
 #parses package CSV file and loads it into Package Class
-def parsePackages(packageFile):
-    with open(packageFile) as packCSV:
+def parsePackages(package_file):
+    with open(package_file) as packCSV:
         packageInfo = csv.reader(packCSV)
         next (packageInfo, None)
         for package in packageInfo:
             packID = int(package[0])
-            packAddress = package[1]
-            packCity = package[2]
-            packZip = package[4]
-            packDeadline = package[5]
-            packWeight = package[6]
-            packStatus = "At WGU shipment facility"
+            pack_address = package[1]
+            pack_city = package[2]
+            pack_zip = package[4]
+            pack_deadline = package[5]
+            pack_weight = package[6]
+            pack_status = "At WGU shipment facility"
 
             #puts csv packages into Package class and then into packageTable
-            pack = Package(packID, packAddress, packCity, packZip,packDeadline, packWeight, packStatus)
+            pack = Package(packID, pack_address, pack_city, pack_zip,pack_deadline, pack_weight, pack_status)
             packageTable.insert(packID, pack)
 
 
@@ -135,9 +137,9 @@ class Truck:
 
 #TODO: call trucks to begin delivering
 
-truck1 = Truck("At WGU facility", "8:00 AM", [1,2,4,5,7,8,10,11,12,17,21,22,23,24,26,27])
-truck2 = Truck("At WGU facility", "8:00 AM", [3,13,14,15,16,18,19,20,29,30,31,33,34,35,36,38])
-truck3 = Truck("At WGU facility", "10:00 AM", [6,9,25,28,32,37,39,40])
+truck1 = Truck(addressDict[1], "8:00 AM", [1,2,4,5,7,8,10,11,12,17,21,22,23,24,26,27])
+truck2 = Truck(addressDict[1], "8:00 AM", [3,13,14,15,16,18,19,20,29,30,31,33,34,35,36,38])
+truck3 = Truck(addressDict[1], "10:00 AM", [6,9,25,28,32,37,39,40])
 
 #TODO: create output for user interface
 
@@ -145,3 +147,4 @@ truck3 = Truck("At WGU facility", "10:00 AM", [6,9,25,28,32,37,39,40])
 parsePackages("CSVdata/packages_CSV.csv")
 
 print(packageTable.get(1))
+print(addressDict[1])
